c6423dfed9708f78947b55bd58723dfce5cea6b2
lesliesage <leslie.sage@gmail.com>
2019-04-04 15:45:32 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+  if num == 2
+    ans = true
+  elsif num < 2
+    ans = false
+  else
+    test_array = (2..(num/2+1)).to_a
+    test_array.each do |testval|
+      if num % testval != 0
+        ans = true
+      else ans = false
+        break
+      end
+    end
+  end
+  return ans
+end# Add  code here!
